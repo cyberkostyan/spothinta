13c6299594da56057bae2512b981d917a0207adc
kfromlarkit <kk@larkit.org>
2026-08-21 20:35:04 +0300

    Redirect old vercel.app domain to spothinta.app

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const createNextIntlPlugin = require("next-intl/plugin")
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts")
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "finland-electricity-prices.vercel.app" }],
+        destination: "https://spothinta.app/:path*",
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = withNextIntl(nextConfig)
